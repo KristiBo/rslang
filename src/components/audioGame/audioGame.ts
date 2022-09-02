ab1b9c43695765204c3f2fe4226a9f99afcce9a4
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-09-03 02:53:25 +0900

    feat: add results after game and add restart game button

--- src/components/audioGame/audioGame.ts
+++ src/components/audioGame/audioGame.ts
@@ -19,6 +19,8 @@ class AudioGame extends NewElem {
 
   private audioElem: Sound | null = null;
 
+  private wordText: HTMLElement | null = null;
+
   private counter: HTMLElement | null = null;
 
   private answerButtonsContainer: HTMLElement | null = null;
@@ -28,6 +30,8 @@ class AudioGame extends NewElem {
   private nextButton: HTMLButtonElement | null = null;
 
   private words: string[][] = [];
+
+  private initialWords: Word[] = [];
 
   private wordIdx = 0;
 
@@ -53,12 +57,13 @@ class AudioGame extends NewElem {
 
   private startGame(): void {
     const closeBtn = new NewElem(this.gameDiv, 'div', 'audio-game__close').elem;
-    closeBtn.innerHTML = SVG(ICON.CLOSE);
+    closeBtn.innerHTML = `<a href="#/games">${SVG(ICON.CLOSE)}</a>`;
     this.audioIcon = new NewElem(this.gameDiv, 'div', 'audio-game__icon').elem;
     this.audioElem = new Sound(this.audioIcon, 'audio-game__sound', `${URL}${null}`);
     this.initAudioListener(this.audioIcon, this.audioElem);
     const audioSvg = new NewElem(this.audioIcon, 'div', 'audio-game__svg').elem;
     audioSvg.innerHTML = SVG(ICON.SPEAKER);
+    this.wordText = new NewElem(this.gameDiv, 'div', 'audio-game__word').elem;
     const counterDiv = new NewElem(this.gameDiv, 'div', 'audio-game__counter-wrapper').elem;
     this.counter = new NewElem(counterDiv, 'div', 'audio-game__counter').elem;
     this.answerButtonsContainer = new NewElem(this.gameDiv, 'div', 'audio-game__buttons').elem;
@@ -75,7 +80,8 @@ class AudioGame extends NewElem {
   }
 
   setWords(words: Word[]): void {
-    const wordsArray = words.map((item) => [item.wordTranslate, item.audio]);
+    this.initialWords = words;
+    const wordsArray = words.map((item) => [item.wordTranslate, item.audio, item.word, '']);
     wordsArray.sort(() => Math.random() - 0.5).length = 20; // shuffle array
     this.words = wordsArray;
   }
@@ -94,14 +100,6 @@ class AudioGame extends NewElem {
     }, 1000);
   }
 
-  // private initValues(): void {
-  //   // remove non-audio content
-  //   for (let i = this.gameDiv.childNodes.length - 1; i >= 0; i -= 1) {
-  //     if (this.gameDiv.childNodes[i].nodeName !== 'AUDIO') this.gameDiv.childNodes[i].remove();
-  //   }
-  //   this.wordIdx = 0;
-  // }
-
   private drawStartCountdown(): void {
     const counter = new NewElem(this.gameDiv, 'p', 'window__counter counter_begin').elem;
     this.sound.start.run();
@@ -113,6 +111,7 @@ class AudioGame extends NewElem {
     (this.counter as HTMLElement).textContent = `${this.counterNumber}`;
     this.counterNumber -= 1;
     (this.nextButton as HTMLButtonElement).textContent = "I don't know";
+    (this.wordText as HTMLElement).innerText = '';
     const set = new Set();
     const amountOfAnswers = 5;
     this.indexOfRightAnswer = Math.floor(Math.random() * (5 - 0)) + 0;
@@ -129,17 +128,19 @@ class AudioGame extends NewElem {
         this.audioElem.run();
       }
       this.answerBtns.forEach((btn, index) => {
+        console.log(this.answerBtns);
         btn.disabled = false;
         btn.classList.remove('correct', 'incorrect');
         if (index === this.indexOfRightAnswer) {
           btn.textContent = this.words[this.wordIdx][0];
         } else {
           const indexOfIncorrectAnswer = arrayOfIncorrectIndexes.pop() as number;
+          console.log(indexOfIncorrectAnswer);
           btn.textContent = this.words[indexOfIncorrectAnswer][0];
         }
       });
     } else {
-      console.log('game end');
+      this.endGame();
     }
   }
 
@@ -149,15 +150,18 @@ class AudioGame extends NewElem {
       if (btn.textContent === this.words[this.wordIdx][0]) {
         btn.classList.add('correct');
         this.sound.right.run();
+        this.words[this.wordIdx][3] = 'right';
       } else {
         btn.classList.add('incorrect');
         this.answerBtns[this.indexOfRightAnswer].classList.add('correct');
         this.sound.wrong.run();
+        this.words[this.wordIdx][3] = 'wrong';
       }
       this.answerBtns.forEach((button) => {
         button.disabled = true;
       });
       (this.nextButton as HTMLButtonElement).textContent = 'Next word';
+      (this.wordText as HTMLElement).innerText = `${this.words[this.wordIdx][2]}`;
     };
 
     this.answerBtns?.forEach((btn) => {
@@ -177,7 +181,9 @@ class AudioGame extends NewElem {
     const btn = e.target as HTMLButtonElement;
     if (btn.textContent === "I don't know") {
       this.sound.click.run();
+      (this.wordText as HTMLElement).innerText = `${this.words[this.wordIdx][2]}`;
       this.showCorrectAnswer();
+      this.words[this.wordIdx][3] = 'wrong';
     } else {
       this.wordIdx += 1;
       this.drawWord();
@@ -190,6 +196,75 @@ class AudioGame extends NewElem {
 
   private initAudioListener(elem: HTMLElement, snd: Sound): void {
     elem.addEventListener('click', () => snd.run());
+  }
+
+  private endGame(): void {
+    const rightAnswers = this.words.filter((item) => item[3] === 'right').length;
+    const wrongAnswers = this.words.filter((item) => item[3] === 'wrong').length;
+    this.sound.end.run();
+    let _: HTMLElement;
+    this.gameDiv.innerHTML = '';
+    this.gameDiv.classList.add('nogap');
+    const resTable = new NewElem(this.gameDiv, 'div', 'window__table').elem;
+    if (rightAnswers) {
+      _ = new NewElem(resTable, 'div', 'table__header header_right', `Правильных ответов: ${rightAnswers}`).elem;
+      this.words.forEach((item, index) => {
+        if (this.words[index][3] === 'right') {
+          this.drawTableRow(resTable, index, 'row_green');
+        }
+      });
+    }
+    if (wrongAnswers) {
+      _ = new NewElem(resTable, 'div', 'table__header header_wrong', `Неправильных ответов: ${wrongAnswers}`).elem;
+      this.words.forEach((item, index) => {
+        if (this.words[index][3] === 'wrong') {
+          this.drawTableRow(resTable, index, 'row_red');
+        }
+      });
+    }
+    const resBtn = new Button(this.gameDiv, 'Играть заново', 'window__btn btn btn_replay').elem;
+    this.dispatchGameStatisticEvent();
+    this.initReplayListener(resBtn);
+  }
+
+  private dispatchGameStatisticEvent(): void {
+    const game = 'sprint';
+    const words = this.words;
+    const event = new CustomEvent('gameStatistic', { detail: { game, words } });
+    document.dispatchEvent(event);
+  }
+
+  private initReplayListener(elem: HTMLElement): void {
+    elem.addEventListener('click', () => {
+      this.sound.click.run();
+      this.setWords(this.initialWords);
+      this.initValues();
+      this.gameDiv.classList.remove('nogap');
+      this.drawStartCountdown();
+    });
+  }
+
+  private initValues(): void {
+    // remove non-audio content
+    for (let i = this.gameDiv.childNodes.length - 1; i >= 0; i -= 1) {
+      if (this.gameDiv.childNodes[i].nodeName !== 'AUDIO') this.gameDiv.childNodes[i].remove();
+    }
+    this.wordIdx = 0;
+    this.indexOfRightAnswer = 0;
+    this.counterNumber = 20;
+    this.answerBtns = [];
+  }
+
+  private drawTableRow(node: HTMLElement, index: number, css: string): void {
+    const row = new NewElem(node, 'div', `table__row ${css}`).elem;
+    // audio cell
+    const audioDiv = new NewElem(row, 'div', 'row__audio').elem;
+    audioDiv.innerHTML = SVG(ICON.SPEAKER);
+    const snd = new Sound(audioDiv, 'audio__sound', `${URL}${this.words[index][1]}`);
+    this.initAudioListener(audioDiv, snd);
+    // word & translation cells
+    let _ = new NewElem(row, 'div', 'row__word', `${this.words[index][2]}`).elem;
+    _ = new NewElem(row, 'div', 'row__translation', `${this.words[index][0]}`).elem;
   }
 }
 
