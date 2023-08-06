e5ff03d2c04c888e361c75de5ca9bae5968bf53f
KristiBo <kristina.boroh@gmail.com>
2023-08-06 22:44:52 +0300

    fix: add new url

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
-const URL = 'https://react-learnwords-example.herokuapp.com/';
+const URL = 'https://rslang-bpf0.onrender.com/';
 const FOURHOURS = 14400; // 60 sec * 60 min * 4 hrs
 const pageColors = ['#e7d3ff', '#cce0ff', '#d3ffe4', '#ffffce', '#ffe1bb', '#fddfe4', '#fccdcd'];
 export { URL, FOURHOURS, pageColors };
